694834252885e44d7d45c9933e529b61ed3fd771
17p1017-hash <17p1017@stu.meisei-u.ac.jp>
2026-09-15 12:08:22 +0900

    Update sounds.js

--- sounds.js
+++ sounds.js
@@ -4,14 +4,23 @@
 ========================================================= */
 
 const SOUND_LIST = [
+
   {
     id: "su",
     label: "ス",
     file: "./words/su.js"
   },
+
   {
     id: "ji",
     label: "ジ",
     file: "./words/ji.js"
+  },
+
+  {
+    id: "tsu",
+    label: "ツ",
+    file: "./words/tsu.js"
   }
+
 ];
