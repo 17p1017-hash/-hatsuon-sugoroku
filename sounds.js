b5d0221ed505e4ede64d037db26f3f36b1f16678
17p1017-hash <17p1017@stu.meisei-u.ac.jp>
2026-09-17 11:45:11 +0900

    Update sounds.js

--- sounds.js
+++ sounds.js
@@ -2,6 +2,7 @@ const SOUND_LIST = [
   { id: "su", label: "ス", file: "./words/su.js" },
   { id: "ji", label: "ジ", file: "./words/ji.js" },
   { id: "tsu", label: "ツ", file: "./words/tsu.js" },
+  { id: "ka", label: "カ", file: "./words/ka.js" },
   {
     id: "rya-ryu-ryo",
     label: "リャ・リュ・リョ",
