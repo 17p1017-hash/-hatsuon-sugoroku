b2aa74519d036ab6f23e0b0dbc80742b70a4bfd2
17p1017-hash <17p1017@stu.meisei-u.ac.jp>
2026-09-17 11:16:54 +0900

    Update rya-ryu-ryo.js

--- words/rya-ryu-ryo.js
+++ words/rya-ryu-ryo.js
@@ -1,1 +1,38 @@
+window.WORD_DATA = {
+  target: "リャ・リュ・リョ",
 
+  groups: {
+    rya: [
+      { word: "リャマ", image: "🦙" }
+    ],
+
+    ryu: [
+      { word: "りゅう", image: "🐉" },
+      { word: "きょうりゅう", image: "🦖" },
+      { word: "りゅうせい", image: "🌠" },
+      { word: "りゅうぐうじょう", image: "🏯" },
+      { word: "りゅうぐうのつかい", image: "🐟" },
+      { word: "リュック", image: "🎒" },
+      { word: "リュックサック", image: "🎒" },
+      { word: "いちりゅう", image: "🥇" }
+    ],
+
+    ryo: [
+      { word: "りょこう", image: "🧳" },
+      { word: "りょかん", image: "♨️" },
+      { word: "りょうり", image: "🍳" },
+      { word: "りょうりにん", image: "👨‍🍳" },
+      { word: "りょうて", image: "🙌" },
+      { word: "りょうあし", image: "🦵" },
+      { word: "りょうし", image: "🎣" },
+      { word: "りょうしん", image: "👨‍👩‍👧" },
+      { word: "しゃりょう", image: "🚃" },
+      { word: "ざいりょう", image: "🥕" },
+      { word: "しょくりょう", image: "🍚" },
+      { word: "いんりょう", image: "🥤" },
+      { word: "むりょう", image: "🆓" },
+      { word: "ゆうりょう", image: "💴" },
+      { word: "たいりょう", image: "🐟" }
+    ]
+  }
+};
