ce6690960b84aa2372af13b46a5df992ca330b3f
17p1017-hash <17p1017@stu.meisei-u.ac.jp>
2026-09-17 11:17:44 +0900

    Update sounds.js

--- sounds.js
+++ sounds.js
@@ -1,26 +1,10 @@
-/* =========================================================
-   発音すごろく
-   遊べるターゲット音の一覧
-========================================================= */
-
 const SOUND_LIST = [
-
+  { id: "su", label: "ス", file: "./words/su.js" },
+  { id: "ji", label: "ジ", file: "./words/ji.js" },
+  { id: "tsu", label: "ツ", file: "./words/tsu.js" },
   {
-    id: "su",
-    label: "ス",
-    file: "./words/su.js"
-  },
-
-  {
-    id: "ji",
-    label: "ジ",
-    file: "./words/ji.js"
-  },
-
-  {
-    id: "tsu",
-    label: "ツ",
-    file: "./words/tsu.js"
+    id: "rya-ryu-ryo",
+    label: "リャ・リュ・リョ",
+    file: "./words/rya-ryu-ryo.js"
   }
-
 ];
